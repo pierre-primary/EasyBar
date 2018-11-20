a00cc9bd0395674bc8097e985dc6269d38aba5b2
PY-Jiang <Y-bao@users.noreply.github.com>
2018-11-21 03:26:44 +0800

    Update dist/easy-bar.esm.js

--- dist/easy-bar.esm.js
+++ dist/easy-bar.esm.js
@@ -1,5 +1,5 @@
 /*!
-* easy-bar.js v1.0.1
+* easy-bar.js v1.0.2
 * (c) 2018-2018 PengYuan-Jiang
 */
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
